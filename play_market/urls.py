560e561e689b0c2ae032912b4db8e5223a19e88e
Svyatoslav Cherednichenko <s.cherednichenko@bytesarea.com.ua>
2018-09-28 15:21:07 +0300

    added ForseCrawlView to manualy kick cralw into action with api request

--- play_market/urls.py
+++ play_market/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 
 from rest_framework.routers import DefaultRouter
 
-from play_market.views import CategoryViewSet, AppViewSet
+from play_market.views import CategoryViewSet, AppViewSet, ForseCrawlView
 
 router = DefaultRouter()
 router.register(r'category', CategoryViewSet)
@@ -10,4 +10,5 @@ router.register(r'app', AppViewSet)
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
+    url(r'^api/force_crawl', ForseCrawlView.as_view()),
 ]
--- play_market/views.py
+++ play_market/views.py
@@ -1,8 +1,21 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 
 from play_market.models import Category, App
 from play_market.serializers import CategorySerializer, AppSerializer
+from play_market.tasks import crawl_play_market
+
+
+class ForseCrawlView(APIView):
+    """
+    View that invokes celery task which runs crawl_play_market command
+    """
+
+    def get(self, request):
+        crawl_play_market.delay()
+        return Response("Work in progress...")
 
 
 class CategoryViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
